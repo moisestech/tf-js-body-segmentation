20a5620753cb9a134aa85a46362643f05aa13b01
moisestech <m@moises.tech>
2021-02-14 09:02:23 -0500

    init react react-dom imp styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import "./styles.css";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
 let project_name = "Tensorflow.js React Body Segmentation";
 
 console.log(project_name);
+
+// https://www.youtube.com/watch?v=KAaXbGr9o0s
